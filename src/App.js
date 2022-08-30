bfad7d5348e38161f4372ca553b3f741e31d70ef
Adenike Ogunmodede <adenicks727@gmail.com>
2022-08-30 15:48:56 +0100

    style changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [characterCount, setCharacterCount] = useState(0);
   const [sentenceCount, setSentenceCount] = useState(0);
   const [userInput, setUserInput] = useState("");
+  const [whiteSpace, setWhiteSpace] = useState(0);
 
   function handleChange(event) {
     setUserInput(event.target.value);
@@ -16,21 +17,27 @@ function App() {
       let words = event.target.value.split(" ");
       return words.filter((word) => word !== "").length;
     });
-    // setLineCount(() => {
-    //   let text = event.target.value.split("/n").length;
-    //   return text;
-    // });
+    setLineCount(() => {
+      let line = event.target.value.split("\n");
+      return line.filter((line) => line !== "").length;
+    });
     setSentenceCount(() => {
       let sentence = event.target.value.split(".");
       return sentence.filter((sentence) => sentence !== "").length;
     });
+    // setWhiteSpace(() => {
+    //   let whiteSpace = event.target.value.split("");
+    //   return whiteSpace.filter((whiteSpace) => whiteSpace !== " ").length;
+    // });
   }
   return (
     <Fragment>
       <div className="background">
         <div className="header">
           <nav className="navbar">
-            <p>Word Counter</p>
+            <p>
+              Word <span>Counter</span>
+            </p>
             <div className="navlink">
               <ul>
                 <li>
@@ -50,20 +57,23 @@ function App() {
           <div className="counter">
             <p>
               Characters
-              <br />
               <span className="count">{characterCount}</span>
             </p>
             <p>
-              Words <br />
+              Words
               <span className="count">{wordCount}</span>
             </p>
             <p>
               Lines
-              <br /> <span className="count">{lineCount}</span>
+              <span className="count">{lineCount}</span>
             </p>
             <p>
-              Sentence <br /> <span className="count">{sentenceCount}</span>
+              Sentence<span className="count">{sentenceCount}</span>
             </p>
+            {/* <p>
+              White-Space
+              <br /> <span className="count">{whiteSpace}</span>
+            </p> */}
           </div>
           <form>
             <div>
